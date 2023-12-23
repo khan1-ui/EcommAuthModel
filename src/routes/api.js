054c7fb84b3e7c328e7618c7ac7e92402db6315a
khan1-ui <143926320+khan1-ui@users.noreply.github.com>
2023-12-23 19:23:46 +0600

    second

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controller/userController')
 const AuthVerification = require('../middleware/authVerification')
+const ProductController = require('../controllers/ProductController');
 const wishListController = require('../controller/wishListController')
 
 //users api ending point
@@ -12,6 +13,18 @@ router.get('/userLogout',AuthVerification,userController.userLogout)
 router.post('/createProfile',AuthVerification,userController.createProfile)
 router.post('/updateProfile',AuthVerification,userController.updateProfile)
 router.get('/readProfile',AuthVerification,userController.readProfile)
+
+//product
+router.get('/ProductBrandList',ProductController.ProductBrandList)
+router.get('/ProductCategoryList',ProductController.ProductCategoryList)
+router.get('/ProductSliderList',ProductController.ProductSliderList)
+router.get('/ProductListByBrand/:BrandId',ProductController.ProductListByBrand)
+router.get('/ProductListByCategory/:CategoryId',ProductController.ProductListByCategory)
+router.get('/ProductListByKeyword/:KeywordId',ProductController.ProductListByKeyword)
+router.get('/ProductListByRemark/:RemarkId',ProductController.ProductListByRemark)
+router.get('/ProductListBySimilar/:SimilarId',ProductController.ProductListBySimilar)
+router.get('/ProductDetails/:DetailsId',ProductController.ProductDetails)
+router.get('/ProductReviewList/:ReviewId',ProductController.ProductReviewList)
 
 //wish list api ending point
 router.post('/saveWishList',AuthVerification,wishListController.saveWishList)
